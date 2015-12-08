5515a8d85c8bb26fba1a7fae40647164f75e953b
cabosanlucas <kaju99@gmail.com>
2015-12-08 00:26:16 -0800

    Tuned after rahuls upload. Also fixed .project issues

---
+++ SDR/src/org/usfirst/frc/team649/robot/OI.java
@@ -0,0 +1,20 @@
+package org.usfirst.frc.team649.robot;
+
+import org.usfirst.frc.team649.robot.commands.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+    //// CREATING BUTTONS
+ 
+    public OI() {
+     	SmartDashboard.putData("Drive 10", new Drive(10));
+    	SmartDashboard.putData("Drive -10", new Drive(-10));
+    }
+
+}
+
